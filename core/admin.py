30ade95c78c3608f7176ca53fca599b4b7f73215
HenriqueCoelho1 <alleson_henrique@hotmail.com>
2022-01-01 23:28:48 -0300

    Admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Product, Costumer
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("name", "price", "stock")
+
+
+class CostumerAdmin(admin.ModelAdmin):
+    list_display = ("name", "surname", "email")
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Costumer, CostumerAdmin)
--- core/models.py
+++ core/models.py
@@ -6,8 +6,14 @@ class Product(models.Model):
     price = models.DecimalField(decimal_places=2, max_digits=9)
     stock = models.IntegerField()
 
+    def __str__(self):
+        return f"{self.name} with {self.stock}"
+
 
 class Costumer(models.Model):
     name = models.CharField(max_length=250)
     surname = models.CharField(max_length=250)
     email = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return f"{self.name} with {self.surname}"
