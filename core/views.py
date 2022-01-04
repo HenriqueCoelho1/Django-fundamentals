d444c19de6db3ebde7cf1a4fe4890c64e7a591e3
HenriqueCoelho1 <alleson_henrique@hotmail.com>
2022-01-04 19:16:21 -0300

    page 404, logout redirect

--- core/views.py
+++ core/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Product
 
+from django.http import HttpResponse
+from django.template import loader
+
 
 def index_page(request):
     print(f"User {request.user}")
@@ -19,3 +22,17 @@ def product_page(request, id):
     product = get_object_or_404(Product, id=id)
     context = {"product": product}
     return render(request, "product.html", context)
+
+
+def error404(request, ex):
+    template = loader.get_template("404.html")
+    return HttpResponse(
+        content=template.render(), content_type="text/html; charset=utf8", status=404
+    )
+
+
+def error500(request):
+    template = loader.get_template("500.html")
+    return HttpResponse(
+        content=template.render(), content_type="text/html; charset=utf8", status=500
+    )
